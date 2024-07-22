794d560ec94e02668ff30c38550ab18642f7ccc9
Jason Madar <jmadar@gmail.com>
2024-07-22 23:13:18 +0000

    removing all dependencies to the original bumble code, only use tensorflow

--- app.py
+++ app.py
@@ -1,27 +1,112 @@
-import inference
-from private_detector.utils.preprocess import preprocess_for_evaluation
-
+from typing import Any, Tuple
 from flask import Flask, request, jsonify, send_from_directory
 
-inference.tf.get_logger().setLevel('ERROR')
-inference.absl_logging.set_verbosity(inference.absl_logging.ERROR)
+import tensorflow as tf
 
-model = inference.tf.saved_model.load('./saved_model')
+tf.get_logger().setLevel('ERROR')
+
+
+def pad_resize_image(image: tf.Tensor,
+                     dims: Tuple[int, int]) -> tf.Tensor:
+    """
+    Resize image with padding
+
+    Parameters
+    ----------
+    image : tf.Tensor
+        Image to resize
+    dims : Tuple[int, int]
+        Dimensions of resized image
+
+    Returns
+    -------
+    image : tf.Tensor
+        Resized image
+    """
+    image = tf.image.resize(
+        image,
+        dims,
+        preserve_aspect_ratio=True
+    )
+
+    shape = tf.shape(image)
+
+    sxd = dims[1] - shape[1]
+    syd = dims[0] - shape[0]
+
+    sx = tf.cast(
+        sxd / 2,
+        dtype=tf.int32
+    )
+    sy = tf.cast(
+        syd / 2,
+        dtype=tf.int32
+    )
+
+    paddings = tf.convert_to_tensor([
+        [sy, syd - sy],
+        [sx, sxd - sx],
+        [0, 0]
+    ])
+
+    image = tf.pad(
+        image,
+        paddings,
+        mode='CONSTANT',
+        constant_values=128
+    )
+
+    return image
+
+
+def preprocess_for_evaluation(image: tf.Tensor,
+                              image_size: int,
+                              dtype: tf.dtypes.DType) -> tf.Tensor:
+    """
+    Preprocess image for evaluation
+
+    Parameters
+    ----------
+    image : tf.Tensor
+        Image to be preprocessed
+    image_size : int
+        Height/Width of image to be resized to
+    dtype : tf.dtypes.DType
+        Dtype of image to be used
+
+    Returns
+    -------
+    image : tf.Tensor
+        Image ready for evaluation
+    """
+    image = pad_resize_image(
+        image,
+        [image_size, image_size]
+    )
+
+    image = tf.cast(image, dtype)
+
+    image -= 128
+    image /= 128
+
+    return image
+
+model = tf.saved_model.load('./saved_model')
 def predict(image_b64):
 
-    image = inference.tf.io.decode_base64(image_b64)
-    image = inference.tf.io.decode_jpeg(image, channels=3)
+    image = tf.io.decode_base64(image_b64)
+    image = tf.io.decode_jpeg(image, channels=3)
 
     image = preprocess_for_evaluation(
         image,
         480,
-        inference.tf.float16
+        tf.float16
     )
 
-    image = inference.tf.reshape(image, -1)
+    image = tf.reshape(image, -1)
 
     preds = model([image])
-    return inference.tf.get_static_value(preds[0])[0]
+    return tf.get_static_value(preds[0])[0]
 
 
 app = Flask(__name__)
